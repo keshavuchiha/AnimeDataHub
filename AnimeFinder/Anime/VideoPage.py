2624aeba44ee5caae6790e5b0968461ea2dfcd64
keshav <keshavgoel_2k19co186@dtu.ac.in>
2022-07-30 17:03:41 +0530

    moreinfo

--- AnimeFinder/Anime/VideoPage.py
+++ AnimeFinder/Anime/VideoPage.py
@@ -10,8 +10,21 @@ class VideoPage():
     def __init__(self,driver:WebDriver):
         self.driver=driver
     
+    def getEpisodeCount(self):
+        self.epCount=self.driver.find_element(By.XPATH,'//span[@id="epsavailable"]').get_attribute('innerHTML')
+        print(self.epCount)
+        pass
+    def getComments(self):
+        commentBtn=self.driver.find_element(By.XPATH,'//button[@id="showcommentbtn"]')
+        self.driver.execute_script('arguments[0].scrollIntoView(true);',commentBtn)
+        self.driver.execute_script('arguments[0].click();',commentBtn)
+        pass
     def Video(self):
-        
+        pass
+    def moreInfo(self):
+        moreInfoBtn=self.driver.find_element(By.XPATH,'//*[text()="More info"]')
+        self.driver.execute_script('arguments[0].scrollIntoView(true);',moreInfoBtn)
+        self.driver.execute_script('arguments[0].click();',moreInfoBtn)
         
 
 
--- AnimeFinder/Anime/listPage.py
+++ AnimeFinder/Anime/listPage.py
@@ -1,11 +1,13 @@
 
 from time import sleep
+import time
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from Anime.getLinks import Links
+from Anime.VideoPage import VideoPage
 class ListPage():
     def __init__(self,driver:WebDriver):
         self.driver=driver
@@ -16,7 +18,7 @@ class ListPage():
         # sleep(2)
         print(f'//button[text()="{char}"]')
         xpath=f'//button[text()="{char}"]'
-        WebDriverWait(self.driver,timeout= 15).until(EC.presence_of_all_elements_located((By.XPATH,'//div[@id="listplace"]/div')))
+        WebDriverWait(self.driver,timeout= 20).until(EC.presence_of_all_elements_located((By.XPATH,'//div[@id="listplace"]/div')))
         print('waited')
         link=self.driver.find_element(By.XPATH,xpath)
         link.click()
@@ -26,9 +28,27 @@ class ListPage():
         # //div[@id="listplace"]/div[text()="GOGO"]/a[not(contains(@title,"Dub"))]
         subAnime=self.driver.find_elements(By.XPATH,'//div[@id="listplace"]/div[text()="GOGO"]/a[not(contains(@title,"Dub"))]')
         for anime in subAnime:
-            print(anime.get_attribute('innerHTML'))
+            # print(anime.get_attribute('innerHTML'))
+            # btn
             if(anime.get_attribute('innerHTML')=='Kimetsu no Yaiba'):
-                anime.click()
+                
+                # 
+                # self.driver.execute_script('arguments[0].scrollIntoView(true);',anime)
+                # WebDriverWait(self.driver,timeout= 15).until(EC.element_to_be_clickable((By.XPATH,'//div[@id="listplace"]/div[text()="GOGO"]/a[not(contains(@title,"Dub")) and text()="Kimetsu no Yaiba"]')))
+                
+                # btn=self.driver.find_element(By.XPATH,'//div[@id="listplace"]/div[text()="GOGO"]/a[not(contains(@title,"Dub")) and text()="Kimetsu no Yaiba"]')
+                # self.driver.execute_script('arguments[0].click();',anime)
+                # self.driver.get(anime.get_attribute('href'))
+                print(anime.get_attribute('href'))
+                # anime.click()
+                # time.sleep(5)
+                btn=anime
+                # self.driver.get(anime.get_attribute('href'))
+        self.driver.execute_script('arguments[0].scrollIntoView(true);',btn)
+        self.driver.execute_script('arguments[0].click();',btn)
+        videoPage=VideoPage(self.driver)
+        # videoPage.getEpisodeCount()
+        videoPage.moreInfo()
         
 
 
