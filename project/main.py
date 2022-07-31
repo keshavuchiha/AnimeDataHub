d1dbdce4f29d5a5eabb64c73eccaf60cfe915eb0
keshav <keshavgoel_2k19co186@dtu.ac.in>
2022-07-31 21:15:41 +0530

    deleted temp files

--- project/main.py
+++
@@ -1,9 +0,0 @@
-import imp
-import time
-from AnimeProject.anime import Anime
-
-with Anime() as bot:
-        bot.landingPage()
-        print('exicting...')
-        # time.sleep(100)
-        input('continue')
